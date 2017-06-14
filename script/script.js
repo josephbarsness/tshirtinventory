d62c8a8abbae140f66064b2b545c585925543cdc
Joseph Barsness <joseph.barsness@gmail.com>
2017-06-14 18:01:58 -0500

    added ng controller

--- script/script.js
+++ script/script.js
@@ -1,1 +1,10 @@
 console.log('js');
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('indexController',['$scope', '$http', function($scope, $http){
+console.log('in indexController');
+
+
+
+}]); // end indexController
